453ad08e9f53d76240e8ef8bae4bacc33e0012dd
Aziz Bekkine <abekkine@gmail.com>
2018-04-21 01:07:02 +0300

    Planets separated into a solar system class (See #107)

--- universe.h
+++ universe.h
@@ -7,7 +7,7 @@
 #include <thread>
 
 #include "space_ship.h"
-#include "planet.h"
+#include "solar_system.h"
 #include "effects_manager.h"
 #include "object_manager.h"
 #include "collision_handler.h"
@@ -16,10 +16,9 @@
 class Universe {
 public:
     Universe()
-    : kNumPlanets(5)
-    , world_(0)
+    : world_(0)
+    , solar_system_(0)
     , space_ship_(0)
-    , planets_(0)
     , effects_(0)
     , state_(GameDefinitions::gameState_InMenu)
     {}
@@ -29,8 +28,8 @@ public:
         //        : + effects_
         //        : + world_
         //        : + collision_handler_
+        //        : + solar_system_
         delete space_ship_;
-        delete [] planets_;
 
         thread_.join();
     }
@@ -39,15 +38,11 @@ public:
         state_ = state;
     }
 
-    struct CelestialBodyDef {
-        double x, y;
-        double angle;
-        double radius;
-        double coreRadius;
-        double angularVelocity;
-        double color;
-    };
     void Init() {
+        // No gravity, since gravity will be modeled manually.
+        b2Vec2 gravity(0.0f, 0.0f);
+        world_ = new b2World(gravity);
+
         effects_ = new EffectsManager();
         effects_->Init();
         OBJMGR.Set("effects", effects_);
@@ -58,44 +53,16 @@ public:
         space_ship_->SetAngle(0.0);
         OBJMGR.Set("ship", space_ship_);
 
-        // Instantiate planets.
-        planets_ = new Planet[kNumPlanets];
-
-        CelestialBodyDef u[] = {
-            {      0.0,      0.0, 0.0,  100.0,  99.7,  0.20,  435.0 }, // Earth
-            {    600.0,      0.0, 0.0,   25.0,  24.9,  0.01, 2730.0 }, // Moon
-            { -19900.0, -11500.0, 0.0, 1000.0, 100.0,  1.00, 3984.0 }, // Sun
-            {  -4900.0, -11500.0, 0.0,   90.0,  89.9,  0.10, 1742.0 }, // Venus
-            {  13000.0, -11500.0, 0.0,   50.0,  49.9,  0.15, 3172.0 }  // Mars
-        };
-
-        for (int i=0; i<kNumPlanets; ++i) {
-            planets_[i].SetPosition(u[i].x, u[i].y);
-            planets_[i].SetAngle(u[i].angle);
-            planets_[i].SetRadius(u[i].radius);
-            planets_[i].SetCoreRadius(u[i].coreRadius);
-            planets_[i].SetAngularVelocity(u[i].angularVelocity);
-            planets_[i].SetColor(u[i].color);
-        }
-
-        // [TODO] : Planets & numPlanets would be passed inside a single object.
-        OBJMGR.Set("planets", planets_);
-        OBJMGR.Set("nplanets", (void *)&kNumPlanets);
-
-        // Initialize physics.
-        // No gravity, since gravity will be modeled manually.
-        b2Vec2 gravity(0.0f, 0.0f);
-        world_ = new b2World(gravity);
-        collision_handler_ = new CollisionHandler();
-        world_->SetContactListener(collision_handler_);
-
-        // Planet creation
-        for(int i=0; i<kNumPlanets; ++i) {
-            planets_[i].Init(world_);
-        }
+        // Instantiate planets & solar system.
+        solar_system_ = new SolarSystem();
+        solar_system_->Init(world_);
 
         // Player creation
         space_ship_->Init(world_);
+
+        // Initialize physics.
+        collision_handler_ = new CollisionHandler();
+        world_->SetContactListener(collision_handler_);
 
         // Initialize timer.
         timer_.Init();
@@ -105,11 +72,7 @@ public:
         thread_ = std::thread(&Universe::ThreadLoop, this);
     }
     void Render() {
-        for (int i=0; i<kNumPlanets; ++i) {
-            glPushMatrix();
-            planets_[i].Render();
-            glPopMatrix();
-        }
+        solar_system_->Render();
     }
 
 private:
@@ -122,9 +85,7 @@ private:
 
             space_ship_->Update(delta_time);
 
-            for (int i=0; i<kNumPlanets; ++i) {
-                planets_[i].Update();
-            }
+            solar_system_->Update(delta_time);
 
             effects_->Update(delta_time);
 
@@ -142,21 +103,16 @@ private:
     }
     void UpdateGravity() {
         // Gravity
-        b2Vec2 g = b2Vec2(0.0, 0.0);
-        for (int i=0; i<kNumPlanets; ++i) {
-
-            g += planets_[i].GetGravityAcceleration(space_ship_->GetPosition());
-        }
+        b2Vec2 g = solar_system_->GetGravityAcceleration(space_ship_->GetPosition());
         space_ship_->SetGravityAcceleration(g);
     }
 
 private:
-    const int kNumPlanets;
     b2World * world_;
 
     std::thread thread_;
+    SolarSystem * solar_system_;
     SpaceShip * space_ship_;
-    Planet * planets_;
     EffectsManager * effects_;
     CollisionHandler * collision_handler_;
     GameDefinitions::GameStateEnum state_;
